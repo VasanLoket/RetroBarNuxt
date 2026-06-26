5bf747f2def4d26a116afcbcfc421fdc493df704
VasanLoket <vasan343300@gmail.com>
2026-06-26 18:14:45 +0300

    Fix fatal error

---
+++ pages/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <!-- Первый блок -->
+  <section class="page-section page-bg-1">
+    <div class="main-content">
+      <h1 class="main-title">Retrowave Bar</h1>
+    </div>
+  </section>
+
+  <!-- Второй блок -->
+  <section class="page-section page-bg-2 second-screen">
+    <div class="grid-container">
+      <div class="card about-card">
+        Уютный бар для GEEKов и для тех, кто скучает по посиделкам с друзьями у сеги 👾
+        - холодное пенное
+        - редкий крафт
+        - ретро-игры
+        - отличные закуски
+        - отличная компания
+        #олдытут! Приходи с друзьями и окунись в атмосферу 80х-90х-00х годов
+      </div>
+      <div class="card photo-card">Фото 1</div>
+      <div class="card photo-card">Фото 2</div>
+      <div class="card photo-card">Фото 3</div>
+      <div class="card photo-card">Фото 4</div>
+    </div>
+  </section>
+
+  <!-- Третий блок -->
+  <section class="page-section page-bg-3 third-screen">
+    <div class="grid-container-beer">
+      <div class="card photo-card">Фото 1</div>
+      <div class="card photo-card">Фото 2</div>
+      <div class="card photo-card">Фото 3</div>
+      <div class="card photo-card">Фото 4</div>
+      <div class="beer-block beer-block--list">
+        <h3>🍺 Пиво — всерьёз:</h3>
+        <ul>
+          <li>❄️ только холодное (никакого «комнатного»);</li>
+          <li>🔥 редкий крафт, который меняется каждые 2–3 недели;</li>
+          <li>🍟 сытные закуски под любой вкус;</li>
+          <li>👾 а главное — компания, с которой приятно разделить и пенное, и воспоминания о 16‑битной эпохе.</li>
+        </ul>
+      </div>
+    </div>
+  </section>
+
+  <!-- Четвёртый блок -->
+  <section class="page-section page-bg-4 fourth-screen">
+    <div class="flex-container-promo">
+      <div class="card promo-card">Акции</div>
+      <div class="card promo-card">Конкурсы и ивенты</div>
+      <div class="card promo-card">Система лояльности</div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
